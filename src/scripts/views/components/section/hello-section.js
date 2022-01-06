b6edf846a0d0d45ee868b6a867a236e8c4ff9cde
Rohman <rohmanpendo@gmail.com>
2022-01-06 17:56:00 +0700

    chore: fix typo

--- src/scripts/views/components/section/hello-section.js
+++ src/scripts/views/components/section/hello-section.js
@@ -8,8 +8,8 @@ class HelloSection extends HTMLElement {
     }
 
     navActiveState() {
-        this.goToPoftfolio = document.getElementById('portfolio-go-to');
-        ActiveNav.click([this.goToPoftfolio]);
+        this.goToPortfolio = document.getElementById('portfolio-go-to');
+        ActiveNav.click([this.goToPortfolio]);
     }
 
     render() {
